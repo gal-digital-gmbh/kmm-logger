a540cbb0b60dde5aa21b79a921a732cf92306d47
Dimitri Pfaffenrodt <dimitri.pfaffenrodt@gal-digital.de>
2021-05-12 17:33:44 +0200

    :arrow_up: upgrade kotlin to 1.5.0

--- buildSrc/src/main/java/Version.kt
+++ buildSrc/src/main/java/Version.kt
@@ -1,7 +1,7 @@
 object Version {
     const val logger = "1.0.1"
 
-    const val kotlin = "1.4.31"
+    const val kotlin = "1.5.0"
     object Android {
         const val minSdk = 24
         const val targetSdk = 30
