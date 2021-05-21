39f1e3ce1f174f2e65960f5f8da020a2174df418
Dimitri Pfaffenrodt <dimitri.pfaffenrodt@gal-digital.de>
2021-05-21 13:06:27 +0200

    :bug: move functions to provide default arguments on swift

--- kmm-logger/src/commonMain/kotlin/de/galdigital/logger/Log.kt
+++ kmm-logger/src/commonMain/kotlin/de/galdigital/logger/Log.kt
@@ -1,12 +1,7 @@
 package de.galdigital.logger
 
-fun info(message: String) = info(message = message, loggerTag = "logger")
 expect fun info(message: String, loggerTag: String = "logger")
 
-fun debug(message: String) = debug(message = message, loggerTag = "logger")
 expect fun debug(message: String, loggerTag: String = "logger")
 
-
-fun logError(message: String) = logError(message = message, loggerTag = "logger")
-fun logError(message: String, throwable: Throwable? = null) = logError(message = message, throwable = throwable, loggerTag = "logger")
 expect fun logError(message: String, throwable: Throwable? = null, loggerTag: String = "logger")
--- kmm-logger/src/iosMain/kotlin/de/galdigital/logger/Log.kt
+++ kmm-logger/src/iosMain/kotlin/de/galdigital/logger/Log.kt
@@ -1,13 +1,17 @@
 package de.galdigital.logger
 
+fun info(message: String) = info(message = message, loggerTag = "logger")
 actual fun info(message: String, loggerTag: String) {
     println("$loggerTag : $message")
 }
 
+fun debug(message: String) = debug(message = message, loggerTag = "logger")
 actual fun debug(message: String, loggerTag: String) {
     println("$loggerTag : $message")
 }
 
+fun logError(message: String) = logError(message = message, loggerTag = "logger")
+fun logError(message: String, throwable: Throwable? = null) = logError(message = message, throwable = throwable, loggerTag = "logger")
 actual fun logError(message: String, throwable: Throwable?, loggerTag: String) {
     println("$loggerTag message : $message")
     println("$loggerTag error : ${throwable?.message}")
